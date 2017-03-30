a71e3edafe470610c8bbbb140df86f63307b7c3f
Piotr Dobrowolski <admin@tastycode.pl>
2017-03-30 13:36:19 +0200

    Initial cx_Freeze installer

---
+++ build.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set innopath="C:\Program Files (x86)\Inno Setup 5"
+set pythonpath="buildreqs/WinPython-32bit-2.7.13.1Zero/python-2.7.13"
+
+set python_exe=%pythonpath%\python.exe
+set iscc_exe=%innopath%\ISCC.exe
+
+echo - install requirements
+%python_exe% -m pip install cx_freeze
+%python_exe% -m pip install -r requirements.txt
+
+echo - distutils cx_freeze build
+%python_exe% setup.py build || goto :error
+
+echo - Inno Setup build
+%iscc_exe% "installer.iss" || goto :error
+
+echo - Finished!
+pause
+exit 0
+
+:error
+echo - Failed!
+pause
+exit 1
---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys
+
+from cx_Freeze import setup, Executable
+
+def get_rev():
+    return open('.git/' + (open('.git/HEAD').read().strip().split(': ')[1])).read()[:7]
+
+build_exe_options = {
+    "include_files": ["icon.ico"],
+    "packages": ["PySide", "bitcoinrpc"],
+    }
+
+build_exe_options["constants"] = [
+    "BUILD_REV='%s'" % get_rev()
+    ]
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(name="ChainSign",
+      version="0.1",
+      description="ChainSign blockchain signing and timestamping app",
+      executables=[Executable("chainsign.py", base=base, icon="icon.ico")],
+      options={"build_exe": build_exe_options})
